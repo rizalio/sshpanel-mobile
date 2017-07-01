80b0381d904d89d50a7e95b87feca18515340ab8
rizalio <admin@rizalfakhri.com>
2017-07-01 15:53:59 +0700

    Fix for logout

--- src/app/logout.global.ts
+++ src/app/logout.global.ts
@@ -1,6 +1,6 @@
 import { Directive, ElementRef, HostListener } from '@angular/core';
 import { Storage } from '@ionic/storage';
-import {AlertController, LoadingController, NavController} from 'ionic-angular';
+import {AlertController, LoadingController, NavController, ToastController} from 'ionic-angular';
 import {DefaultPage} from "../pages/default/default";
 
 
@@ -11,7 +11,7 @@ export class logoutGlobal {
 
   ctrl = null;
 
-  constructor(private el: ElementRef, public storage: Storage, public navCtrl: NavController, public loading: LoadingController, public alertCtrl: AlertController) {
+  constructor(private el: ElementRef, public storage: Storage, public navCtrl: NavController, public loading: LoadingController, public alertCtrl: AlertController, public toast: ToastController) {
 
 
   }
@@ -20,16 +20,20 @@ export class logoutGlobal {
     let data = this.ctrl;
 
     setTimeout(function(){
-      data.storage.remove('user')
-      data.alertCtrl.create({
-        title: 'Sukses!',
-        subTitle: 'Berhasil Logout',
-        buttons: ['OK']
-      });
+      data.storage.remove('user');
       data.navCtrl.setRoot(DefaultPage);
+      data.toast.create({
+        message: 'Berhasil Logout',
+        duration: 3000,
+        position: 'bottom'
+      }).present();
+
+
+
     }, 2000);
     this.loading.create({
-      content: 'Sedang Logout'
+      content: 'Sedang Logout',
+      duration: 2000
     }).present(), data;
 
 
